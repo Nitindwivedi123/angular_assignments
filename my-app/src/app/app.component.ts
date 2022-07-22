8643fdca71fbe21d50f78869af56fef24c41c85d
Nitin bunker <nitindwivedibhu02@gmail.com>
2022-07-22 11:44:53 +0530

    second commit

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  temp=false
+  onClick(){
+    this.temp=true
+  }
+}
---
+++ my-app/src/app/child/child.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  constructor(private appService: AppService) {}
+  data : any
+  ngOnInit(): void {
+    this.appService.getData().subscribe((res) => {
+      this.data= res
+    })
+  }
+  
+}
